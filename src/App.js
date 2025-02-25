1620c4ad0ecced0c9d32308599d479b31dc2ece3
Pallavi <ppagoti@uab.edu>
2025-02-25 14:09:43 -0600

    modified app.js to remove browser router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter className="App">
+    <div className="App">
       <Router />
-    </BrowserRouter>
+    </div>
   );
 }
 
